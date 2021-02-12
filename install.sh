ad76a9db5f6dac7e53ba436309f061db9536f46d
unknown <stealthee.camel@gmail.com>
2021-02-12 23:33:40 +0000

    A usefull message

--- install.sh
+++ install.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
-sudo apt-get -y update
-sudo apt-get -y upgrade
+#based on https://github.com/nahamsec/bbht
+sudo apt -y update
+sudo apt -y upgrade
 
-
-sudo apt-get install -y libcurl4-openssl-dev
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y jq
-sudo apt-get install -y ruby-full
-sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
-sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
-sudo apt-get install -y python-setuptools
-sudo apt-get install -y libldns-dev
-sudo apt-get install -y python3-pip
-sudo apt-get install -y python-pip
-sudo apt-get install -y python-dnspython
-sudo apt-get install -y git
-sudo apt-get install -y rename
-sudo apt-get install -y xargs
 
 echo "installing bash_profile aliases from recon_profile"
 git clone https://github.com/nahamsec/recon_profile.git
@@ -25,7 +11,6 @@ cat bash_profile >> ~/.bash_profile
 source ~/.bash_profile
 cd ~/tools/
 echo "done"
-
 
 
 #install go
@@ -38,9 +23,10 @@ select choice in "${choices[@]}"; do
                 yes)
 
 					echo "Installing Golang"
-					wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
-					sudo tar -xvf go1.13.4.linux-amd64.tar.gz
-					sudo mv go /usr/local
+					#wget https://golang.org/dl/go1.15.7.linux-amd64.tar.gz
+					#sudo tar -xvf go1.15.7.linux-amd64.tar.gz
+					#sudo mv go /usr/local
+					sudo apt install -y golang
 					export GOROOT=/usr/local/go
 					export GOPATH=$HOME/go
 					export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
@@ -73,7 +59,7 @@ mkdir ~/tools
 cd ~/tools/
 
 #install aquatone
-echo "Installing Aquatone"
+echo "Installing Aquatone \n github.com/michenriksen/aquatone"
 go get github.com/michenriksen/aquatone
 echo "done"
 
@@ -82,14 +68,16 @@ echo "Installing Chromium"
 sudo snap install chromium
 echo "done"
 
-echo "installing JSParser"
+
+echo "installing JSParser \n https://github.com/nahamsec/JSParser.git"
 git clone https://github.com/nahamsec/JSParser.git
 cd JSParser*
 sudo python setup.py install
 cd ~/tools/
 echo "done"
 
-echo "installing Sublist3r"
+
+echo "installing Sublist3r \n https://github.com/aboul3la/Sublist3r.git"
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r*
 pip install -r requirements.txt
@@ -97,86 +85,74 @@ cd ~/tools/
 echo "done"
 
 
-echo "installing teh_s3_bucketeers"
+echo "installing teh_s3_bucketeers \n https://github.com/tomdev/teh_s3_bucketeers.git"
 git clone https://github.com/tomdev/teh_s3_bucketeers.git
 cd ~/tools/
 echo "done"
 
 
-echo "installing wpscan"
-git clone https://github.com/wpscanteam/wpscan.git
-cd wpscan*
-sudo gem install bundler && bundle install --without test
-cd ~/tools/
-echo "done"
-
-echo "installing dirsearch"
+echo "installing dirsearch \n https://github.com/maurosoria/dirsearch.git"
 git clone https://github.com/maurosoria/dirsearch.git
 cd ~/tools/
 echo "done"
 
 
-echo "installing lazys3"
+echo "installing lazys3 \n https://github.com/nahamsec/lazys3.git"
 git clone https://github.com/nahamsec/lazys3.git
 cd ~/tools/
 echo "done"
 
-echo "installing virtual host discovery"
+echo "installing virtual host discovery \n https://github.com/jobertabma/virtual-host-discovery.git"
 git clone https://github.com/jobertabma/virtual-host-discovery.git
 cd ~/tools/
 echo "done"
 
 
-echo "installing sqlmap"
-git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-cd ~/tools/
-echo "done"
-
-echo "installing knock.py"
+echo "installing knock.py \n https://github.com/guelfoweb/knock.git"
 git clone https://github.com/guelfoweb/knock.git
 cd ~/tools/
 echo "done"
 
-echo "installing lazyrecon"
+
+echo "installing lazyrecon \n https://github.com/nahamsec/lazyrecon.git"
 git clone https://github.com/nahamsec/lazyrecon.git
 cd ~/tools/
 echo "done"
 
 echo "installing nmap"
-sudo apt-get install -y nmap
+sudo apt install -y nmap
 echo "done"
 
-echo "installing massdns"
-git clone https://github.com/blechschmidt/massdns.git
-cd ~/tools/massdns
-make
-cd ~/tools/
-echo "done"
 
-echo "installing asnlookup"
+echo "installing asnlookup\n https://github.com/yassineaboukir/asnlookup.git"
 git clone https://github.com/yassineaboukir/asnlookup.git
 cd ~/tools/asnlookup
 pip install -r requirements.txt
 cd ~/tools/
 echo "done"
 
-echo "installing httprobe"
+
+echo "installing httprobe \n github.com/tomnomnom/httprobe"
 go get -u github.com/tomnomnom/httprobe 
 echo "done"
 
-echo "installing unfurl"
+
+echo "installing unfurl \n github.com/tomnomnom/unfurl"
 go get -u github.com/tomnomnom/unfurl 
 echo "done"
 
-echo "installing waybackurls"
+
+echo "installing waybackurls \n github.com/tomnomnom/waybackurls"
 go get github.com/tomnomnom/waybackurls
 echo "done"
 
-echo "installing crtndstry"
+
+echo "installing crtndstry \n\ https://github.com/nahamsec/crtndstry.git"
 git clone https://github.com/nahamsec/crtndstry.git
 echo "done"
 
-echo "downloading Seclists"
+
+echo "downloading Seclists \n https://github.com/danielmiessler/SecLists.git"
 cd ~/tools/
 git clone https://github.com/danielmiessler/SecLists.git
 cd ~/tools/SecLists/Discovery/DNS/
@@ -186,7 +162,18 @@ cd ~/tools/
 echo "done"
 
 
+echo "installing  PEAS Suite \n https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git"
+cd ~/tools/
+git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
+echo "done"
+
+
+echo "installing github-search \n https://github.com/gwen001/github-search.git"
+cd ~/tools/
+git clone https://github.com/gwen001/github-search.git
+
 
 echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
 ls -la
 echo "One last time: don't forget to set up AWS credentials in ~/.aws/!"
+echo "TODO: For Nuclei - Download latest binary from https://github.com/projectdiscovery/nuclei/releases"
