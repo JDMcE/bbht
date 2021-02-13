8574e73a41bec1524194716d2591467a454ee6d6
John McEwan <johnd.mcewan@gmail.com>
2021-02-13 20:25:58 +0000

    a usefull message

--- install.sh
+++ install.sh
@@ -12,6 +12,16 @@ source ~/.bash_profile
 cd ~/tools/
 echo "done"
 
+echo "installing bash.rc"
+git clone https://github.com/JDMcE/Config-Files.git
+cd Config-Files
+mv .bashrc ~/.bashrc
+
+echo "installing sublime text"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt install sublime-text
+
 
 #install go
 if [[ -z "$GOPATH" ]];then
