cb07a496dfb169d006e6f3a29c50599c9e985196
John McEwan <johnd.mcewan@gmail.com>
2021-02-13 20:56:03 +0000

    updates

--- install.sh
+++ install.sh
@@ -20,9 +20,11 @@ mv .bashrc ~/.bashrc
 echo "installing sublime text"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
-sudo apt install sublime-text
+sudo apt -y update
+sudo apt install -y sublime-text
 
+echo "installing terminator"
+sudo apt install -y terminator
 
 #install go
 if [[ -z "$GOPATH" ]];then
